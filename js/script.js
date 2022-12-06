5604d09334c96b891152c36bbe27624b42ff22e9
taeheonkang <119912177+taeheonkang@users.noreply.github.com>
2022-12-06 15:21:05 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,107 @@
+$(function(){
+    $(".log_btn") .click(function(){
+        $(".log_box") .fadeIn();
+    });
+
+    $(".log a") .click(function(){
+        $(".log_box") .fadeOut();
+    });
+
+    $(".sign_btn") .click(function(){
+        $(".sign_box") .fadeIn();
+    });
+
+    $(".sign a") .click(function(){
+        $(".sign_box") .fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod1 a").click(function(){
+        $(".popup1").fadeIn();
+    });
+    $(".pbox1 a").click(function(){
+        $(".popup1").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod2 a").click(function(){
+        $(".popup2").fadeIn();
+    });
+    $(".pbox2 a").click(function(){
+        $(".popup2").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod3 a").click(function(){
+        $(".popup3").fadeIn();
+    });
+    $(".pbox3 a").click(function(){
+        $(".popup3").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod4 a").click(function(){
+        $(".popup4").fadeIn();
+    });
+    $(".pbox4 a").click(function(){
+        $(".popup4").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod5 a").click(function(){
+        $(".popup5").fadeIn();
+    });
+    $(".pbox5 a").click(function(){
+        $(".popup5").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod6 a").click(function(){
+        $(".popup6").fadeIn();
+    });
+    $(".pbox6 a").click(function(){
+        $(".popup6").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod7 a").click(function(){
+        $(".popup7").fadeIn();
+    });
+    $(".pbox7 a").click(function(){
+        $(".popup7").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod8 a").click(function(){
+        $(".popup8").fadeIn();
+    });
+    $(".pbox8 a").click(function(){
+        $(".popup8").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod9 a").click(function(){
+        $(".popup9").fadeIn();
+    });
+    $(".pbox9 a").click(function(){
+        $(".popup9").fadeOut();
+    });
+});
+
+$(function(){
+    $(".prod10 a").click(function(){
+        $(".popup10").fadeIn();
+    });
+    $(".pbox10 a").click(function(){
+        $(".popup10").fadeOut();
+    });
+});
